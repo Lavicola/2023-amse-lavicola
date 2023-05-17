db304779f3a8e658e78958062fc075dddae76fee
lavicola <lavicola7777@t-online.de>
2023-05-17 17:14:32 +0200

    feat: add command line arguments
    This Feature adds command line Arguments which give the user more freedom and configuration possibilities in the coming commits:
    --db: user can specify the where the database shall be placed
    --log: user can turn on/off the logging
    --pipeline_intermediate: the pipeline.py uses the intermediate data
    -- store_intermediate: store store_intermediate data into a json file

--- data/pipeline.py
+++ data/pipeline.py
@@ -1,7 +1,10 @@
+import os
+
 import kba_car_registrations
 import ladesauele
 import data_transformer
 import data_saver
+import argparse
 
 
 def main():
@@ -12,21 +15,57 @@ def main():
     Store the Data via data_saver
 
     """
+    # setup constants
+    INTERMEDIATE_FILENAME = "intermediate.json"
+
+    # setup cmd arg parameter
+    parser = argparse.ArgumentParser()
+
+    # arguments:
+    parser.add_argument('--db', type=str, help='The Location the database shall be placed(default: %(default)s)',
+                        default=os.getcwd())
+    parser.add_argument('--log', type=bool,
+                        help='Create a log file to persistently store debug info(default: %(default)s)', default=True)
+    parser.add_argument('--store_intermediate', type=bool, help='Store the extracted data of the "extract & specific data '
+                                                          'transformation" into a file before transforming the data('
+                                                          'default: %(default)s)',
+                        default=False)
+    parser.add_argument('--pipeline_intermediate', type=bool,
+                        help='Use the Intermediate file instead of extracting it from the website(default: %(default)s)',
+                        default=False)
+    args = parser.parse_args()
+
     json_list = []
     json_elements = None
-    print("Starting with downloading the kba car registration Data ")
-    json_elements = kba_car_registrations.main()
-    if json_elements:
-        print("Success, Data found and stored")
-        json_list += json_elements
-    json_list = data_transformer.transform_table_name(json_list)
+    # depending if intermediate file shall be used or not
+    if args.pipeline_intermediate:
+        # read intermediate data
+        intermediate_path = os.path.join(os.getcwd(),INTERMEDIATE_FILENAME)
+        print(f"Checking if {intermediate_path} exists")
+        if not os.path.isfile(intermediate_path):
+            print(f"Intermediate file in place {intermediate_path} not found, abort!")
+            return
+        print("trying to load intermediate data")
+        return
+    else:
+        print("Starting with downloading the kba car registration Data ")
+        json_elements = kba_car_registrations.main()
+        if json_elements:
+            print("Success, Data found and stored")
+            json_list += json_elements
+        json_list = data_transformer.transform_table_name(json_list)
+        print("Downloading the Ladesauelen Data ")
+        json_elements = ladesauele.main()
+        if json_elements:
+            print("Success, Data found and stored")
+            json_list += json_elements
+        print("Starting to Transform the data")
 
-    print("Downloading the Ladesauelen Data ")
-    json_elements = ladesauele.main()
-    if json_elements:
-        print("Success, Data found and stored")
-        json_list+=json_elements
-    print("Starting to Transform the data")
+    if args.store_intermediate:
+        #data_saver.save_intermediate(json_list)
+        return
+
+    # transform and store into database
     json_list = data_transformer.main(json_list)
     print("Storing the Data into a SQLite Database")
     data_saver.main(json_list)
