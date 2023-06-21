096426bd804eea187575d537f31acda5b59ff010
lavicola <lavicola7777@t-online.de>
2023-06-21 02:17:35 +0200

    fix: petrol,diesel etc. are now integer
    I think Integer is the wrong type since it the dot clearly indicates that the value is supposed to be a float, otherwise it would have been a comma.
    (Therefore I deleted my method which determines the best type at runtime)

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,7 +1,4 @@
-import decimal
-import datetime
-import pandas
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 import sqlalchemy.types as types
 import pandas as pd
 
@@ -18,36 +15,6 @@ BYTES = "LargeBinary"
 JSON = "JSON"
 
 
-def python_type_to_sqlalchemy_type(python_type, value):
-    if python_type is int:
-        # Differentiate between BIGINT and INT
-        if value in range(-2147483648, 2147483647):
-            return INTEGER
-        else:
-            return BIGINT
-    elif python_type is float:
-        return FLOAT
-    elif python_type is str:
-        return STRING
-    elif python_type is bool:
-        return types.Boolean
-    # smartass pandas always treats a datetime as Timestamp if to_pydatetime is not called
-    elif python_type is datetime.datetime or pandas.Timestamp:
-        return DATETIME
-    elif python_type is datetime.date:
-        return DATE
-    elif python_type is datetime.time:
-        return DATE
-    elif python_type is decimal.Decimal:
-        return NUMERIC
-    elif python_type is bytes:
-        return BYTES
-    elif python_type is list:
-        return JSON
-    else:
-        raise Exception("error")
-
-
 def clean_validate_part():
     url = r"https://www-genesis.destatis.de/genesis/downloads/00/tables/46251-0021_00.csv"
 
@@ -58,7 +25,7 @@ def clean_validate_part():
     # Ignore the first 6 lines and last 4 lines as metadata
     # Keep only the following columns, rename them to the new name given here (M-BU contain summary data)
     df = pd.read_csv(url, skiprows=7, skipfooter=4, engine='python', encoding="latin1", delimiter=";",
-                     header=None, parse_dates=['date'],converters={'CIN': str},
+                     header=None, parse_dates=['date'], converters={'CIN': str},
                      dayfirst=True, index_col=False,
                      usecols=[0, 1, 2, 12, 22, 32, 42, 52, 62, 72], na_values='-',
                      names=col_names)
@@ -78,41 +45,18 @@ def sql_part(df):
     # Creating the db
     TABLE_NAME = "cars"
     engine = create_engine(f'sqlite:///{TABLE_NAME}.sqlite')
-    results = []
-    # once again at runtime determine the best fit, without relying on checking the data manually
-    # Iterate over the DataFrame columns
-    for column_name, column_values in df.items():
-        current_value_type = None
-        current_column_name = column_name
-        for index, value in column_values.items():
-            value_type = python_type_to_sqlalchemy_type(type(value), value)
-            # first case is current_value_type is None, which is okay, we simply set the value
-            if not current_value_type:
-                current_value_type = value_type
-            # if not None current_value_type is set, and we check if it´s the same value as the value_type,
-            elif current_value_type == value_type:
-                # perfect, just like expected both are the same value
-                continue
-            else:
-                # something happened, the type changed apparently
-                if current_value_type == STRING and value_type == FLOAT:
-                    # our value was String and suddenly we have float --> nan is interpreted as Float, it´s okay if we stay with string
-                    continue
-                else:
-                    raise Exception("whyyyyyyyyyyyy")
-        results.append((current_column_name, current_value_type))
-    create_table_query = f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (\n"
-    for column in results:
-        new_line = f"{column[0]} {column[1]},\n"
-        create_table_query += new_line
-    create_table_query = create_table_query[:-2]
-    create_table_query += ")"
-    #    print(create_table_query)
-    engine = create_engine(f'sqlite:///{TABLE_NAME}.sqlite')
-    with engine.connect() as connection:
-        connection.execute(text(create_table_query))
-    # now insert df
-    df.to_sql(TABLE_NAME, con=engine, if_exists="replace", index=False)
+    df.to_sql(TABLE_NAME, con=engine, if_exists="replace", index=False,
+              dtype={
+                  "date": types.DATETIME,
+                  "CIN": types.String,
+                  "name": types.String,
+                  "petrol": types.Integer,
+                  "diesel": types.Integer,
+                  "gas": types.Integer,
+                  "electro": types.Integer,
+                  "hybrid": types.Integer,
+                  "plugInHybrid": types.Integer,
+                  "others": types.Integer, }, )
     return 0
 
 
